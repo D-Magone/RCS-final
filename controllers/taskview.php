a71257ad647d8e156635f2a3470706b6879decf6
D-Magone <daina.a.magone@gmail.com>
2021-07-01 18:33:47 +0300

    fix foreach issue

--- controllers/taskview.php
+++ controllers/taskview.php
@@ -12,28 +12,32 @@ class ViewTasks {
         $this->datas = new Task();
         $this->datas = $this->datas->getAllTasks();
 
-        foreach ($this->datas as $data) {
-            if ($data['user_id'] == $_SESSION['session_id']) {
-                ?>
-                <tr type="hidden">
-                    <td>
-                        <?php echo $data['description'];?>
-                    </td>
-                    <td>
-                        <form method="POST" action="">
-                            <button class="button-style" id="Edit" type="submit" name="action" value="Edit"><span class="material-icons">edit</span></button>
-                            <button class="button-style" type="submit" name="action" value="Delete"><span class="material-icons">delete</span></button>
-                            
-                            <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
-                            <input type="hidden" name="descrption" value="<?php echo $data['description'] ?>">
-                            
-                        </form>
-                    </td>
-                </tr>
-            <?php
+        if(is_array($this->datas) || is_object($this->datas)) {
+
+        
+            foreach ($this->datas as $data) {
+                if ($data['user_id'] == $_SESSION['session_id']) {
+                    ?>
+                    <tr type="hidden">
+                        <td>
+                            <?php echo $data['description'];?>
+                        </td>
+                        <td>
+                            <form method="POST" action="">
+                                <button class="button-style" id="Edit" type="submit" name="action" value="Edit"><span class="material-icons">edit</span></button>
+                                <button class="button-style" type="submit" name="action" value="Delete"><span class="material-icons">delete</span></button>
+                                
+                                <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
+                                <input type="hidden" name="descrption" value="<?php echo $data['description'] ?>">
+                                
+                            </form>
+                        </td>
+                    </tr>
+                <?php
+                }
             }
+            return;
         }
-        return;
     }
         
 }
